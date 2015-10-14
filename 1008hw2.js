754ffb0b756f16be3d707dc00eebb7dbe7cbb7ae
Lin yu <z2904327@gmail.com>
2015-10-15 00:59:38 +0800

    Update 1008hw2.js

--- 1008hw2.js
+++ 1008hw2.js
@@ -1,3 +1,8 @@
+/*
+Math.random，是隨機亂數取值
+Math.floor，取得小於的最大整數，配合Math.random使用
+。*/
+
 var astr = ["cat","dog","pig","you","I"];
 var bstr = ["kill","love","hate","chase","hug"];
 var cstr = ["cat","dog","pig","teacher","me"];
